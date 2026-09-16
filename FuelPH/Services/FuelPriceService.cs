ff01e7851c9dcced3cc61355ab902a72c48f2a37
Edison Lati Jr. <junior.lati@gmail.com>
2026-09-16 18:17:31 +0800

    FuelPriceService.cs

---
+++ FuelPH/Services/FuelPriceService.cs
@@ -0,0 +1,17 @@
+﻿using FuelPH.DTOs;
+using FuelPH.Services.IServices;
+
+namespace FuelPH.Services
+{
+    public class FuelPriceService : IFuelPriceService
+    {
+        public FuelPriceDto GetDieselPrice()
+        {
+            return new FuelPriceDto
+            {
+                FuelType = "Diesel",
+                PricePerLiter = 57.20m
+            };
+        }
+    }
+}
