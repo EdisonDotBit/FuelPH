758f6e319365f2934baf0792f0e826013cf6ee2f
Edison Lati Jr. <junior.lati@gmail.com>
2026-09-16 15:07:39 +0800

    Add FulePriceDto.cs

---
+++ FuelPH/DTOs/FuelPriceDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FuelPH.DTOs
+{
+    public class FuelPriceDto
+    {
+        public string FuelType { get; set; } = string.Empty;
+        public decimal PricePerLiter { get; set; }
+    }
+}
