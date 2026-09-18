2984ba4d72b7f3cc273f2b90739484e1e121372b
Edison Lati Jr. <junior.lati@gmail.com>
2026-09-18 09:38:23 +0800

    Update method signature in IFuelPriceService.cs
    Make Task promise to return an object implementing IReadOnlyList with a sequence of FuelPriceDto inside the list.

--- FuelPH/Services/IServices/IFuelPriceService.cs
+++ FuelPH/Services/IServices/IFuelPriceService.cs
@@ -4,6 +4,6 @@ namespace FuelPH.Services.IServices
 {
     public interface IFuelPriceService
     {
-        Task<FuelPriceDto> GetDieselPrice();
+        Task<IReadOnlyList<FuelPriceDto>> GetDieselPriceAsync();
     }
 }
