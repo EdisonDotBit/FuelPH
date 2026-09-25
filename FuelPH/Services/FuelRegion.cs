c77cf09c2f3bfec65743e63c2c1d377d87e0a3c7
Edison Lati Jr. <junior.lati@gmail.com>
2026-09-25 11:06:51 +0800

    Add FuelRegion.cs

---
+++ FuelPH/Services/FuelRegion.cs
@@ -0,0 +1,39 @@
+﻿using FuelPH.Models;
+
+namespace FuelPH.Services;
+
+public class RegionService
+{
+    public IReadOnlyList<FuelRegion> GetRegions()
+    {
+        return
+        [
+            new FuelRegion
+            {
+                Name = "Metro Manila",
+                South = 14.60,
+                West = 121.00,
+                North = 14.65,
+                East = 121.05
+            },
+
+            new FuelRegion
+            {
+                Name = "CALABARZON",
+                South = 13.80,
+                West = 120.70,
+                North = 14.30,
+                East = 121.60
+            },
+
+            new FuelRegion
+            {
+                Name = "Central Luzon",
+                South = 14.80,
+                West = 120.30,
+                North = 15.50,
+                East = 121.50
+            }
+        ];
+    }
+}
